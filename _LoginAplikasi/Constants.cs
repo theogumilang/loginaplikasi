a2d622e105595e7c519276e4a8607da5e57360c3
theogumilang <gumilangtheodorus@gmail.com>
2017-07-20 14:42:24 +0700

    Add project files.

---
+++ _LoginAplikasi/Constants.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace _LoginAplikasi
+{
+     class Constants
+    {
+        // APP Requirements
+        public static string AppID = "332063047134905";
+        public static string Scope = "public_profile";
+        public static string CustomerKey = "1Qcd641An54lsJbao0jWgo26P";
+        public static string CustomerSecret = "NCcfQSCjGmVCtGKDpNnm5FUTADppBnryRirQ2vaOccEqJgg5S0";
+
+        // API Definitions
+        public static string FacebookRestAPI = "https://m.facebook.com/dialog/oauth/";
+        public static string LoginSuccessful = "http://www.facebook.com/connect/login_success.html";
+
+        // Response Message
+        public static string ErrorMessage = "Not Authenticated";
+
+        public static string OK = "Ok";
+
+    }
+}
---
+++ _LoginAplikasi/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Auth;
+using System;
+
+namespace _LoginAplikasi
+{
+    [Activity(Label = "_LoginAplikasi", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
+            var facebook = FindViewById<Button>(Resource.Id.fblogin);
+            facebook.Click += delegate { LoginToFacebook(true); };
+
+            Button btnsignup = FindViewById<Button>(Resource.Id.btnsignup);
+            btnsignup.Click += delegate {
+                StartActivity(typeof(SignUpActivity));
+            };
+        }
+        void LoginToFacebook(bool allowCancel)
+        {
+            var auth = new OAuth2Authenticator(
+                clientId: Constants.AppID,
+                scope: Constants.Scope,
+                authorizeUrl: new Uri(Constants.FacebookRestAPI),
+                redirectUrl: new Uri(Constants.LoginSuccessful));
+
+            auth.AllowCancel = allowCancel;
+            // If authorization succeeds or is canceled, .Completed will be fired.
+            auth.Completed += (s, EventArgs) => {
+                if (!EventArgs.IsAuthenticated)
+                {
+                    var builder = new AlertDialog.Builder(this);
+                    builder.SetMessage(Constants.ErrorMessage);
+                    builder.SetPositiveButton(Constants.OK, (o, e) => { });
+                    builder.Create().Show();
+                    return;
+                }
+                
+            };
+            
+            StartActivity(auth.GetUI(this));
+        }
+
+    }
+}
+
